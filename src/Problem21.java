2745626f661e1dd71318f974223c497416ba3e6f
Ning Ning Sun <nsun200@live.com>
2015-09-07 12:44:45 -0400

    solved problem 21

--- src/Problem21.java
+++ src/Problem21.java
@@ -1,20 +1,29 @@
-//Project Euler Probem 21
+//Project Euler Problem 21
 public class Problem21
 {
 	public static void main(String[] args)
 	{
-		
+		int sum = 0;
+		for(int i = 0; i < 10000; i++)
+		{
+			if(i == factorSum(factorSum(i)) && i != factorSum(i))
+			{
+				sum += i;
+			}
+		}
+		System.out.println(sum);
 	}
 	
-	public int factorSum(int n)
+	public static int factorSum(int n)
 	{
 		int sum = 0;
-		for(int i = 1; i <= Math.sqrt(n); i++)
+		for(int i = 1; i <= n/2; i++)
 		{
 			if(n % i == 0)
 			{
 				sum += i;
 			}
 		}
+		return sum;
 	}
 }
